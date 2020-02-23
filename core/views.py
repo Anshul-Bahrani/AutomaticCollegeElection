2e8e577b3d66758552d946709abb8a68f43dd189
Anshul-Bahrani <anshulbahrania@gmail.com>
2020-02-23 05:50:28 +0530

    home page

--- core/views.py
+++ core/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(TemplateView):
 
     template_name = 'pages/home.html'
 
     def get(self, request, *args, **kwargs):
         # pylint: disable=unused-argument
-        context = {'sidebarSection': 'home'}
-        return render(request, self.template_name, context)
+        return render(request, self.template_name)
