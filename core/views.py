e28121afed06d4f51cd2d3f47aabf1019ffa3a07
ahuja devansh <ahuja.devansh2@gmail.com>
2020-02-24 13:07:35 +0530

    priya ko chaye

--- core/views.py
+++ core/views.py
@@ -7,13 +7,14 @@ from ACE.users.models import Student, CustomUser
 import json
 from django.db import connection
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 def my_custom_sql(sql):
     with connection.cursor() as cursor:
         cursor.execute(sql)
         rows = cursor.fetchall()
     return rows
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin ,TemplateView):
     template_name = 'pages/home.html'
     def get(self, request, *args, **kwargs):
         query = 'SELECT dept_name, COUNT(vot_id),MAX(elec_date), post_name FROM ( SELECT dept.id as dept_id, student.id as student_id, ball.voter_id as ball_voter_id, nom.id as nom_id, ball.nominee_id as ball_nominee_id, nom.election_id as nom_election_id, elec.id as elec_id, elec.position_id as elec_position_id, post.id as post_id, dept.name as dept_name, post.name as post_name,ball.id as vot_id , elec.start_at as elec_date FROM academics_department as dept, voting_position as post, voting_ballot as ball, users_customuser as student, voting_nominee as nom, voting_election as elec WHERE dept.id = student.department_id_id AND ball.voter_id = student.id AND nom.id = ball.nominee_id AND nom.election_id = elec.id AND elec.position_id = post.id )as derived GROUP BY dept_name, post_name;'
